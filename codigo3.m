4302bba1d3c50da88936c738d32e01df2d22841e
Stanislav Gatin <155986458+stas-gatin@users.noreply.github.com>
2025-03-16 18:16:06 +0100

    Delete codigo3.m

--- codigo3.m
+++
@@ -1,6 +0,0 @@
-load('modelo_entrenado.mat', 'net');
-YPred = classify(net, imdsValidation);
-YValidation = imdsValidation.Labels;
-
-accuracy = sum(YPred == YValidation) / numel(YValidation);
-disp("Precisión: " + accuracy);
